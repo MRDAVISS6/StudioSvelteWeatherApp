bb6a7265c0d4e307a8ba7a65c8bedd058bcd8e87
Samuel Davies (1000048175) <DAVISS6@student.op.ac.nz>
2025-09-19 14:09:12 +1200

    Changed WeatherData1Day to be a stateful json object and exporting the load function

--- StudioSvelteWeatherApp/src/lib/Components/Weather.svelte
+++ StudioSvelteWeatherApp/src/lib/Components/Weather.svelte
@@ -1,15 +1,27 @@
 <script>
     const apiURL = "https://api.open-meteo.com/v1/forecast?latitude=45.8795&longitude=170.5006&hourly=temperature_2m&timezone=Pacific%2FAuckland&forecast_days=1";
 
+    export const weatherData1Day = $state(
+        {  
+            "loaded": false,
+            "hourly": {
+                "temperature_2m": [],
+                "time": []
+            }
+        }
+    );
 
 	/**
 	 * @param {string | URL | Request} url
 	 */
-    async function LoadWeatherData(url) {
+    export async function LoadWeatherData(url) {
         let reponse = await fetch(url);
         let weatherData = await reponse.json()
             .catch(err => console.error('Error fetching data:', err));
-        return weatherData;
+        
+        weatherData1Day.loaded = true;
+        weatherData1Day.hourly.temperature_2m = weatherData.hourly.temperature_2m;
+        weatherData1Day.hourly.time = weatherData.hourly.time;
     }
-    export const weatherData1Day = LoadWeatherData(apiURL);
+    
 </script>
